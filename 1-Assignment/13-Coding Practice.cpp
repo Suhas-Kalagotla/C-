fae73d3aa07bce90f80c128531d70e9c60f9114f
Suhas-Kalagotla <suhaskalagotla5@gmail.com>
2022-04-30 22:51:06 +0530

    Added the question ...

--- 1-Assignment/13-Coding Practice.cpp
+++ 1-Assignment/13-Coding Practice.cpp
@@ -1,3 +1,14 @@
+/*Write a Program to convert given number of days to a
+measure of time given in years, weeks and days.
+For example 375 days is equal to 1 year 1 week and 3 days (ignore leap year )
+
+Sample I/O:
+
+Enter the number of days
+1000
+1000 days has 2 years, 38 weeks and 4 days
+*/
+
 #include <iostream>
 using namespace std;
 
@@ -11,5 +22,5 @@ int main(){
 
   cout << no_days << " days has " << year << " years, "
   << week << " weeks and " << days << " days";
-  return 0 ; 
+  return 0 ;
 }
