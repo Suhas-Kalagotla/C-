accbbf84560c4272bf8c7a0ccb0f72eae6636d99
Suhas-Kalagotla <suhaskalagotla5@gmail.com>
2022-04-30 22:59:27 +0530

    Converting Fahrenheit to Celsius.

---
+++ 1-Assignment/14-Coding Practice.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(){
+  float fah{};
+  cout << "Enter a temperature in Fahrenheit:\n";
+  cin >> fah;
+
+  float cel = (fah - 32.0 )* (5.0/9.0);
+
+  cout <<fixed << setprecision(1)<< fah << " degree Fahrenheit is equal to " << cel
+  << " degree Celsius";
+  return 0;
+}
